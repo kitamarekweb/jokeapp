53d028696c1ce9ae575d514c0decf96d7ec6fef7
kitamarekweb <kitamarekweb@gmail.com>
2019-12-30 11:21:22 +0100

    t-81. spring XML configuration example. creating XML configuration file.

--- src/main/java/guru/springframework/joke/jokeapp/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -4,10 +4,10 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration //wylaczamy bo teraz przerzucamy sie na xml-owa configurajce i trzeba bylo wylaczy zeby spring nie widzial tej klasy jako configuracji
 public class ChuckConfiguration {
 
-    @Bean
+   //@Bean //wylaczamy bo teraz przerzucamy sie na xml-owa configurajce i trzeba bylo wylaczy zeby spring nie widzial tej klasy jako configuracji
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
